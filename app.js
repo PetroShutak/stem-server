1acfe2ec108bae6fcddb34815f7f1c51f61271d9
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-12-27 19:13:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("public", { maxAge: "31536000" }));
 
 app.use("/api/products", productRouter);
 
