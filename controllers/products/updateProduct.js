2638d5a1fc1cd60cc4b0388e9689456957ebe0cf
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2024-01-12 20:28:46 +0200

    added update route

---
+++ controllers/products/updateProduct.js
@@ -0,0 +1,48 @@
+const { Product } = require("../../models/product");
+
+const updateProduct = async (req, res) => {
+  try {
+    const productId = req.params.id; // Assuming you have the product ID in the request parameters
+
+    // Check if the product exists
+    const existingProduct = await Product.findById(productId);
+    if (!existingProduct) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
+    // Update the product properties based on the request body
+    const {
+      name,
+      brand,
+      model,
+      description,
+      price,
+      category,
+      subcategory,
+      image,
+      rating,
+      country,
+    } = req.body;
+
+    existingProduct.name = name || existingProduct.name;
+    existingProduct.brand = brand || existingProduct.brand;
+    existingProduct.model = model || existingProduct.model;
+    existingProduct.description = description || existingProduct.description;
+    existingProduct.price = price || existingProduct.price;
+    existingProduct.category = category || existingProduct.category;
+    existingProduct.subcategory = subcategory || existingProduct.subcategory;
+    existingProduct.image = image || existingProduct.image;
+    existingProduct.rating = rating || existingProduct.rating;
+    existingProduct.country = country || existingProduct.country;
+
+    // Save the updated product in the database
+    const updatedProduct = await existingProduct.save();
+
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    console.error("Error updating product:", error);
+    res.status(500).json({ error: "Internal server error while updating" });
+  }
+};
+
+module.exports = { updateProduct };
--- routes/api/products.js
+++ routes/api/products.js
@@ -1,16 +1,22 @@
-const express = require('express');
-const router = express.Router(); 
-const { addProduct } = require('../../controllers/products/addProduct'); 
+const express = require("express");
+const router = express.Router();
+const { addProduct } = require("../../controllers/products/addProduct");
+const { updateProduct } = require("../../controllers/products/updateProduct");
 
-const { getProductById, getAllProducts } = require('../../controllers/products/getProducts');
+const {
+  getProductById,
+  getAllProducts,
+} = require("../../controllers/products/getProducts");
 
 // Отримання всіх продуктів
-router.get('/', getAllProducts);
+router.get("/", getAllProducts);
 
 // Отримання конкретного продукту за його ID
-router.get('/:productId', getProductById);
+router.get("/:productId", getProductById);
 
 // Роут для додавання продукту
-router.post('/', addProduct);
+router.post("/", addProduct);
+// Роут для оновлення продукту
+router.patch("/:productId", updateProduct);
 
 module.exports = router;
