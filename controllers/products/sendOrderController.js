6bbd189a337960ac6c467e30e408a6bf800114c1
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2024-01-24 20:41:29 +0200

    Update sendOrderController.js

--- controllers/products/sendOrderController.js
+++ controllers/products/sendOrderController.js
@@ -3,10 +3,10 @@ const sendOrder = require("../../utils/sendOrder");
 const placeOrder = async (req, res) => {
   try {
     // Отримати дані з запиту (ім'я, кількість, адреса, спосіб доставки)
-    const { name, quantity, address, deliveryMethod } = req.body;
+    const { name, phone, payment, delivery, deliveryAddress, postOffice } = req.body;
 
     // Викликати функцію для відправки замовлення
-    await sendOrder({ name, quantity, address, deliveryMethod });
+    await sendOrder({ name, phone, payment, delivery, deliveryAddress, postOffice });
 
     // Відповісти на клієнта
     res.status(200).json({ message: "Order placed successfully!" });
