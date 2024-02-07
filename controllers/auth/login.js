6d2883a2d75b6d015a3eba8eb7b8e8e41278a440
Petro Shutak <petro.shutak.ua@gmail.com>
2024-02-07 15:37:55 +0200

    /* fix */

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -10,17 +10,17 @@ const login = async (req, res) => {
   const user = await User.findOne({ email });
 
   if (!user) {
-    throw new HttpError(401, "Email or password is wrong");
+    throw HttpError(401, "Email or password is wrong");
   }
   
   if (!user.verify) {
-    throw new HttpError(401, "Email not verified");
+    throw HttpError(401, "Email not verified");
   }
 
   const passwordCompare = await bcrypt.compare(password, user.password);
 
   if (!passwordCompare) {
-    throw new HttpError(401, "Email or password is wrong");
+    throw HttpError(401, "Email or password is wrong");
   }
 
   const filteredUser = {
--- controllers/auth/verifyEmail.js
+++ controllers/auth/verifyEmail.js
@@ -7,7 +7,7 @@ const verifyEmail = async (req, res) => {
   const user = await User.findOne({ verificationToken });
 
   if (!user) {
-    throw new HttpError(404, "User not found");
+    throw HttpError(404, "User not found");
   }
 
   await User.findByIdAndUpdate(user._id, {
